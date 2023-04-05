116f8a2b3e338d0a203b771928a39d45bdede1dd
Thabiso <940303tabiso@gmail.com>
2023-04-05 06:05:59 +0000

    MySQL Avanced

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- lists all bands with Glam rock
+-- Column names must be: band_name and lifespan (in years)
+
+SELECT
+band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC;
