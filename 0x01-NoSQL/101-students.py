fa2a4191821fc9a212633a9e6d2060e823b00665
Thabiso <940303tabiso@gmail.com.com>
2023-04-11 13:44:41 -0700

    Advanced

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
-""" Module for using PyMongo """
+""" Top students """
 
 
 def top_students(mongo_collection):
-    """ Returns all students sorted by average score"""
-    return list(mongo_collection.find())
+    """ returns all students sorted by average score """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
